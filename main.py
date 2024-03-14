ce34425b0fd1e4c22a1f3854bfa31ab13e96f055
ilias-anouar <115695877+ilias-anouar@users.noreply.github.com>
2024-03-14 16:32:04 +0000

    finishing the API structure

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
+import json
+import random
+
+range = {"start": 0, "end": 350}
 
 app = FastAPI()
 
@@ -10,6 +14,26 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/api/v1/quotes/{option}")
+def read_random(option: str, id: Union[int, None] = None):
+    f = open("./data/quotes.json")
+    data = json.load(f)
+    if option == "random":
+        f.close()
+        return [data[random.randrange(range["start"], range["end"])]]
+    elif option == "get":
+        if id != None and id <= range["end"]:
+            f.close()
+            return [data[id]]
+        else:
+            f.close()
+            return {"Error": "the quote's id not defined"}
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8200)
